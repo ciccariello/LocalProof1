81a6a181449cbe0f3cd39a7b90edb1bf9e4391f8
myp14 <myp14@DESKTOP-0UR4DGS.fritz.box>
2025-03-10 12:05:38 +0100

    initial commit of proof

---
+++ proof/src/config.h
@@ -0,0 +1,2 @@
+#define primo_VERSION_MAJOR 1
+#define primo_VERSION_MINOR 0
---
+++ proof/src/proof.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : proof.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+#include "config.h"
+#include <iostream>
+using namespace std;
+
+int main() {
+	std::cout << "Hello World" << std::endl;
+	std::cout << "Version " << primo_VERSION_MAJOR << "." << primo_VERSION_MINOR << std::endl;
+
+	return 0;
+}
